ed2499790c6b4d07e16c611bc0882cc19fd9ce7b
shadowops-security <vaibhavrpatil.1520@gmail.com>
2026-01-18 19:43:08 +0530

    Initial portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/vaibhav-portfolio/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
