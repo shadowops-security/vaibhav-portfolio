7fd11699faff087d24d04c5440cb954cf6f99c00
shadowops-security <vaibhavrpatil.1520@gmail.com>
2026-01-18 19:13:12 +0530

    Fix GitHub Pages 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { HashRouter } from 'react-router-dom';
 
+createRoot(document.getElementById('root')!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
 createRoot(document.getElementById("root")!).render(<App />);
