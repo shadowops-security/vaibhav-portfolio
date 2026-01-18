b291d125e933a86c2210df225e99b572718c314b
shadowops-security <vaibhavrpatil.1520@gmail.com>
2026-01-18 19:18:33 +0530

    Fix duplicate React root render

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { HashRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <HashRouter>
     <App />
   </HashRouter>
 );
-createRoot(document.getElementById("root")!).render(<App />);
